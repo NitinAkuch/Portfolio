1bf2f2cd56f42355a49a4ff2d58ce2c54b924163
NitinAkuch <110688610+NitinAkuch@users.noreply.github.com>
2024-08-09 12:14:31 +0530

    FD

--- portfolio/src/pages/index.js
+++ portfolio/src/pages/index.js
@@ -8,7 +8,6 @@ import { LinkArrow } from "@/components/Icons";
 import HireMe from "@/components/HireMe";
 import lightBulb from "../../public/images/svgs/miscellaneous_icons_1.svg";
 
-
 export default function Home() {
   return (
     <>
@@ -38,14 +37,14 @@ export default function Home() {
                 className="!text-6xl !text-left xl:!text-5xl lg:!text-center lg:!text-6xl md:!text-5xl sm:!text-3xl  "
               />
               <p className="my-4 text-base font-medium md:text-sm sm:text-xs ">
-                As a skilled full-stack Java developer, I am dedicated to turning
-                ideas into innovative web and enterprise applications. Explore my latest
-                projects and articles, showcasing my expertise in MERN, Java and Web
-                development.
+                As a skilled full-stack Java developer, I am dedicated to
+                turning ideas into innovative web and enterprise applications.
+                Explore my latest projects and articles, showcasing my expertise
+                in MERN, Java and Web development.
               </p>
               <div className="flex items-center self-start mt-2 lg:self-center">
                 <Link
-                  href="NitinAkuchCV.pdf"
+                  href="NitinAkuchCV_FD1.pdf"
                   target="_blank"
                   className="flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light
                   md:p-2 md:px-4 md:text-base
