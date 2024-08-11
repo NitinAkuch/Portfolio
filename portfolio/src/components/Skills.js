9096f1234bc6c3cb14414c6464d1f0cdfa8e7acc
NitinAkuch <110688610+NitinAkuch@users.noreply.github.com>
2024-08-11 18:12:49 +0530

    Skills

--- portfolio/src/components/Skills.js
+++ portfolio/src/components/Skills.js
@@ -23,12 +23,14 @@ const Skills = () => {
       <h2 className="font-bold text-8xl mt-64 w-full text-center md:text-6xl md:mt-32">
         Skills
       </h2>
-      <div className="w-full h-screen relative flex items-center justify-center rounded-full bg-circularLight dark:bg-circularDark
+      <div
+        className="w-full h-screen relative flex items-center justify-center rounded-full bg-circularLight dark:bg-circularDark
       lg:h-[80vh] sm:h-[60vh] xs:h-[50vh] 
       lg:bg-circularLightLg lg:dark:circularDarkLg 
       md:bg-circularLightMd md:dark:circularDarkMd 
       sm:bg-circularLightSm sm:dark:circularDarkSm
-      ">
+      "
+      >
         <motion.div
           className="flex items-center justify-center rounded-full font-semibold bg-dark text-light py-3 px-6 shadow-dark cursor-pointer  dark:text-dark dark:bg-light
           lg:p-6 md:p-4 xs:text-xs xs:p-2
@@ -42,13 +44,13 @@ const Skills = () => {
         <Skill name={"AWS"} x="-32vw" y="3vw" />
         <Skill name={"ReactJS"} x="0vw" y="12vw" />
         <Skill name={"React-Redux"} x="22vw" y="8vw" />
-        <Skill name={"NextJS"} x="-20vw" y="-15vw" />
+        <Skill name={"JAVA 8"} x="-20vw" y="-15vw" />
         <Skill name={"MySQL"} x="-35vw" y="-10vw" />
-        <Skill name={"NodeJS"} x="15vw" y="-13vw" />
+        <Skill name={"Micro Services"} x="15vw" y="-13vw" />
         <Skill name={"Firebase"} x="30vw" y="-15vw" />
         <Skill name={"CSS"} x="13vw" y="0vw" />
-        <Skill name={"ExpressJS"} x="31vw" y="-5vw" />
-        <Skill name={"TypeScript"} x="0vw" y="-20vw" />
+        <Skill name={"Spring Framework"} x="31vw" y="-5vw" />
+        <Skill name={"Hibernate"} x="0vw" y="-20vw" />
         <Skill name={"MongoDB"} x="-25vw" y="18vw" />
         <Skill name={"SpringBoot"} x="18vw" y="17vw" />
       </div>
